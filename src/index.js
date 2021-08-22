dcaf289c840cb15bf6336b785443289866b8576b
MalboroAdvanced <brijesh.1995rana@gmail.com>
2021-08-22 15:32:51 +0530

    cleaned

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import ApolloProvider from "./ApolloProvider";
 
-ReactDOM.render(ApolloProvider, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
