9e88e580c951e555f762ace0cf7acaa70a4901f3
Brijesh Rana <brijesh.1995rana@gmail.com>
2021-09-03 15:58:08 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -12,3 +12,47 @@ it("should render Ticket and Footer", () => {
   expect(footer.exists()).toBe(true);
   expect(ticket.exists()).toBe(true);
 });
+
+
+FullStack React JS Developer  
+$8.00/hr 
+If you need someone determined to complete the job, then continue reading.
+
+I am a passionate Fullstack Stack Developer with over four years of rich experience.
+
+Communication with me is simple, and you are free to call me anytime you want.
+
+I am always thinking about performance optimization, scalability, and improving workflow for each project. Maintaining clean, reusable, and straightforward code allows me to build stable and sustainable applications.
+
+Responsive, pixel-perfect, cross-browser are my everyday tasks. I have a good eye for UI/UX.
+
+I have strong roots in:
+
+Basics:
+• HTML5 / CSS3 / JS / jQuery
+• TypeScript / ES6 / ES7
+• Bootstrap / Tailwind / Material UI / Semantic UI
+• BEM / Modular development
+
+Preprocessors:
+• Jade / EJS / Haml
+• Sass / Less / Stylus
+
+Frameworks:
+• Next.js / React / Redux
+• D3.js / C3.js
+• MERN stack (React / Node.js / Express.js / MongoDB / Firebase )
+
+Integrations:
+• API / Ajax / Json / WebSockets
+• AWS / Heroku
+• Customer.io / MailChimp
+
+Testing:
+• Jest / Enzyme
+• BrowserStack / CrossBrowserTesting
+
+
+If you are looking for a great experienced developer, get in touch.
+
+Cheers!
